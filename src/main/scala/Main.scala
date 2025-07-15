07e11c2d6c6e0ad35c0702f9dd50bfdca6e364ef
sidendev <96108993+sidendev@users.noreply.github.com>
2025-07-15 22:23:24 +0100

    Add while loop to main so app runs continious till exit

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -2,17 +2,21 @@ object Main {
   def main(args: Array[String]): Unit = {
     println("Welcome to the Morse Code Translator!")
 
-    val input = InputHandler.readInput("Enter text or Morse code to translate (or type 'exit' to quit):")
+    while (true) {
+      val input = InputHandler.readInput("Enter text or Morse code to translate (or type 'exit' to quit):")
 
-    if (InputHandler.isExit(input)) {
-      println("Goodbye!")
-      sys.exit()
-    } else if (InputHandler.isMorse(input)) {
-      val result = Translator.morseCodeToEnglish(input)
-      OutputHandler.printOutput("Text", result)
-    } else {
-      val result = Translator.englishToMorseCode(input)
-      OutputHandler.printOutput("Morse Code", result)
+      if (InputHandler.isExit(input)) {
+        println("Goodbye!")
+        sys.exit()
+      } else if (InputHandler.isMorse(input)) {
+        val result = Translator.morseCodeToEnglish(input)
+        OutputHandler.printOutput("Text", result)
+      } else {
+        val result = Translator.englishToMorseCode(input)
+        OutputHandler.printOutput("Morse Code", result)
+      }
+
+      println()
     }
   }
 }
